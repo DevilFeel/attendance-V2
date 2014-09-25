92d9776f100e02df88ee504a98ea850e45514cb8
Yuyang Zhang <zhangyuyang@live.cn>
2014-09-25 22:09:00 +0800

    更换数据库的连接方式，使用tomcat数据源

--- src/hyit/app/dbc/DatabaseConnection.java
+++ src/hyit/app/dbc/DatabaseConnection.java
@@ -1,14 +1,20 @@
 package hyit.app.dbc;
 
-
 import java.sql.Connection;
-import java.sql.DriverManager;
+//import java.sql.DriverManager;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 
 public class DatabaseConnection {
-	private static final String DBDRIVER = "com.mysql.jdbc.Driver";
-	private static final String DBURL = "jdbc:mysql://localhost:3306/attendanceV2";
-	private static final String DBUSER = "zyy";
-	private static final String DBPASSWORD = "kurimu";
+	private static final String DSNAME = "java:comp/env/jdbc/myDS";
+	/*
+	 * private static final String DBDRIVER = "com.mysql.jdbc.Driver"; private
+	 * static final String DBURL = "jdbc:mysql://localhost:3306/attendanceV2";
+	 * private static final String DBUSER = "zyy"; private static final String
+	 * DBPASSWORD = "kurimu"; for mysql
+	 */
 	/*
 	 * private static final String DBDRIVER = "com.mysql.jdbc.Driver"; //for SAE
 	 * private static final String DBURL =
@@ -20,8 +26,11 @@ public class DatabaseConnection {
 
 	public DatabaseConnection() throws Exception {
 		try {
-			Class.forName(DBDRIVER);
-			this.conn = DriverManager.getConnection(DBURL, DBUSER, DBPASSWORD);
+			/*Class.forName(DBDRIVER);
+			this.conn = DriverManager.getConnection(DBURL, DBUSER, DBPASSWORD);*/
+			Context ctx = new InitialContext();	//使用tomcat数据源
+			DataSource ds = (DataSource)ctx.lookup(DSNAME);
+			this.conn = ds.getConnection();
 		} catch (Exception e) {
 			throw e;
 		}
